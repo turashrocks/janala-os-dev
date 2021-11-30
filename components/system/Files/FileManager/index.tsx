94e1e7e9bd5df962c585c97dd0c2a63ba04c96a7
Willkommen <turash.chowdhury@gmail.com>
2021-12-01 01:09:40 +0600

    FileManager error solved

--- components/system/Files/FileManager/index.tsx
+++ components/system/Files/FileManager/index.tsx
@@ -57,6 +57,10 @@ const FileManager = ({
   const { StyledFileEntry, StyledFileManager } = FileManagerViews[view];
   const { isSelecting, selectionRect, selectionStyling, selectionEvents } =
     useSelection(fileManagerRef);
+  // const fileDrop = useFileDrop({
+  //   callback: folderActions.newPath,
+  //   directory: url,
+  // });
   const fileDrop = useFileDrop({ callback: folderActions.newPath });
   const folderContextMenu = useFolderContextMenu(url, folderActions);
   const loading = (!hideLoading && isLoading) || url !== currentUrl;
